2d206c5d9bdccb234cd37a09ce6233fb50419fe1
Jérémy <chabert.jeremy@gmail.com>
2021-10-26 10:29:02 +0200

    feat(OAuth0): add external authentication service

--- srv/config/config.js
+++ srv/config/config.js
@@ -1,3 +1,7 @@
 module.exports = {
   logLevel: process.env.logLevel,
-}
+  baseURL: process.env.baseURL,
+  secret: process.env.secret,
+  clientID: process.env.clientID,
+  issuerBaseURL: process.env.issuerBaseURL,
+};
--- srv/server.js
+++ srv/server.js
@@ -1,5 +1,8 @@
 const cds = require('@sap/cds');
 const cors = require('cors');
+const config = require('./config/config');
+const { auth, requiresAuth } = require('express-openid-connect');
+const express = require('express');
 module.exports = cds.server;
 
 async function getPlotChart(req, res, next) {
@@ -23,11 +26,27 @@ async function getPlotChart(req, res, next) {
   res.send(positions);
 }
 
+const settings = {
+  authRequired: false,
+  auth0Logout: true,
+  authorizationParams: { // required to retrieve JWT including permissions (our roles)
+    response_type: "code",
+    scope: "openid",
+    audience: "https://my-wine-app-api.com",
+  },
+};
+
 cds.on('bootstrap', (app) => {
   if (process.env.NODE_ENV !== 'production') {
     const cds_swagger = require('cds-swagger-ui-express');
     app.use(cors());
     app.use(cds_swagger());
-  }
+  }  
+  app.use(auth(settings));
   app.get('/getPlotChart', [getPlotChart]);
+  app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
+
+  app.use('/', requiresAuth());  //protect app from root fo
 });
