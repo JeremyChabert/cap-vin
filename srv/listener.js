350e49f949d14d3579965a22224880d10d999093
Jérémy <chabert.jeremy@gmail.com>
2021-10-27 16:08:42 +0200

    core: rename service message > listener

--- srv/listener.js
+++ srv/listener.js
@@ -1,22 +1,27 @@
 const cds = require('@sap/cds');
-const winston = require('./config/winston')
+const winston = require('./config/winston');
 
 module.exports = async (srv) => {
+  const { LogOfEvent } = cds.entities('my.cave');
+
   const retailerService = await cds.connect.to('retailer');
   const customerService = await cds.connect.to('customer');
   //
   //
-  retailerService.on('productAvailable', (msg) => {
+  retailerService.on('productAvailable',  async(msg) => {
     winston.info(['==>> msg received from customerService', msg.event, msg.data]);
+    await INSERT.into(LogOfEvent).entries({ name: msg.event });
   });
   //
   //
-  retailerService.on('productOrdered', (msg) => {
+  retailerService.on('productOrdered', async(msg) => {
     winston.info(['==>> msg received from customerService', msg.event, msg.data]);
+    await INSERT.into(LogOfEvent).entries({ name: msg.event });
   });
   //
   //
-  customerService.on('productSoldOut', (msg) => {
+  customerService.on('productSoldOut',  async(msg) => {
     winston.info(['==>> msg received from retailerService', msg.event, msg.data]);
+     await INSERT.into(LogOfEvent).entries({ name: msg.event });
   });
 };
