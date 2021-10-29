8d34d7ce91a2773992513312e7befd97f1e96891
Jérémy <chabert.jeremy@gmail.com>
2021-10-29 11:06:42 +0200

    feat(position-vizualisation): yet another viz with chartjs

--- srv/server.js
+++ srv/server.js
@@ -1,9 +1,29 @@
 const cds = require('@sap/cds');
+const cors = require('cors');
 module.exports = cds.server;
+
+async function getPlotChart(req, res, next) {
+  const srv = await cds.connect.to('sqlite:db');
+  const { Position } = srv.entities('my.cave');
+  // const createdBy = req.user.id;
+  const positions = await SELECT.from(Position, (a) => {
+    a.positionX,
+      a.positionY,
+      a.cave((b) => {
+        b.ID,
+          b.vin((c) => {
+            c.name;
+          });
+      });
+  });
+  res.send(positions);
+}
 
 cds.on('bootstrap', (app) => {
   if (process.env.NODE_ENV !== 'production') {
     const cds_swagger = require('cds-swagger-ui-express');
+    app.use(cors());
     app.use(cds_swagger());
   }
+  app.get('/getPlotChart', [getPlotChart]);
 });
